1094d5c210167a02f82be31657b00b0257947d65
Ray Swenton <RaymondSwentonJr@gmail.com>
2020-11-10 13:06:59 -0500

    added Expense_Tracker.py Main file, plus 2 methods

---
+++ Expense_Tracker.py
@@ -0,0 +1,17 @@
+class ExpenseTracker():
+    
+    def Subtraction(self,total_amount):
+        ''' Subtracts the amount of each expense from the total_amount_to_spend
+        
+        Attributes:
+            value(float): the amount to be subtracted from total
+            
+        '''
+        
+    def Store_balance(self,total_amount):
+        ''' After user is done, saves the amount_spent to a tuple
+        
+        Side effect:
+            Value of the tuples will change
+        '''    
+        

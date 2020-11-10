2fc37cec2785d733a65a061fd74a59509043d530
Christian Escobar <christianescobar@Christians-MBP.fios-router.home>
2020-11-10 13:32:14 -0500

    added balance_warning and categorize_shopping methods

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -25,4 +25,23 @@ class ExpenseTracker:
         Side effect:
             Value of the tuples will change
         '''    
+     
+    def balance_warning(self,halfway_balance,low_balance):
+        """This method will include a function that will notify the user with a balance warning based on what they set as half and 
+        minimum funds.
         
+        Args:
+            halfway_balance: When the user’s balance is half of what they initially put into the tracker
+			low_balance: When the user’s balance is as low as they indicated to avoid.
+
+		"""
+      
+      
+    def categorize_shopping(self,shopping_list):
+		"""This method will contain a dictionary with categories the user will use to divide who/what to shop for.
+        
+        
+        Args:
+			shopping_list: the dictionary that will add the categories the user inputs.
+
+		"""

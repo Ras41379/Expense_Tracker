c73a9e3e24c29c7e205875a966c34632de0f971a
cheekmsss <cchuku@terpmail.umd.edu>
2020-11-10 13:23:09 -0500

    created init and available funds module

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,4 +1,15 @@
-class ExpenseTracker():
+class ExpenseTracker:
+    '''
+    Program that allows users to track expenses. 
+    '''
+    def __init__(self):
+        '''
+        Initializes the user attribute and tuple that will hold data.
+        '''
+    def available_funds(self):
+        '''
+        Allows user to input available funds
+        '''                                      
     
     def Subtraction(self,total_amount):
         ''' Subtracts the amount of each expense from the total_amount_to_spend

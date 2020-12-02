c1b5363e5c145a317a7ca6400ecd918a5bcb6b22
Ray Swenton <RaymondSwentonJr@gmail.com>
2020-12-02 18:25:37 -0500

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -21,41 +21,45 @@ class ExpenseTracker:
         '''
         
     def Store_balance(self,total_amount):
-        ''' After user is done, saves the amount_spent to a tuple
+        ''' After user is done, saves the amount_spent to a dictionary
+        
+        Attribute:
+            totals (dict): Holds the balance values for the user.
         
         Side effect:
             Value of the tuples will change
         '''  
           
     def print_amount(self,funds):
-        """This method will print the amount of funds remaining after using the funds
+        """This method will print the amount of funds remaining after using the 
+        funds
+        
         Args:
             funds (float): the funds available for use
         Side effect:
             print statement
         """
         
-    def unds(self, funds):
-        """This method will allow the user to update or increase the funds
-        that he/she wishes to be available for use
-        
-        Side Effect:
-            value of the funds will update/increase
-        """
-        
     def balance_warning(self,halfway_balance,low_balance):
-        '''This method will include a function that will notify the user with a balance warning based on what they set as half and 
-        minimum funds.
+        '''This method will include a function that will notify the user with a 
+        balance warning based on what they set as half and minimum funds.
         
         Args:
-            halfway_balance: When the user’s balance is half of what they initially put into the tracker
-            low_balance: When the user’s balance is as low as they indicated to avoid.
+            halfway_balance: When the user’s balance is half of what they 
+            initially put into the tracker
+            low_balance: When the user’s balance is as low as they indicated to 
+            avoid.
 		'''
     
     def categorize_shopping(self,shopping_list):
-        '''This method will contain a dictionary with categories the user will use to divide who/what to shop for 
+        '''
+        This method will contain a dictionary with categories the user will 
+        use to divide who/what to shop for 
         
         
         Args:
-            shopping_list: the dictionary that will add the categories the user inputs.
+            shopping_list: the dictionary that will add the categories the user 
+            inputs.
         '''
+        
+    if __name__ == __main__()

07167058f039381fa7bd9fffedee85f4ce8de46f
Ray Swenton <RaymondSwentonJr@gmail.com>
2020-12-02 17:59:56 -0500

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -2,6 +2,7 @@ class ExpenseTracker:
     '''
     Program that allows users to track expenses. 
     '''
+    
     def __init__(self):
         '''
         Initializes the user attribute and tuple that will hold data.

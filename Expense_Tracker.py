c1a3cd0a23e2cefe7a987fd52aefcac0347eaf85
sjleee03 <s.jungmin03@gmail.com>
2020-12-03 16:35:22 -0500

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -5,7 +5,7 @@ class ExpenseTracker:
     '''
     Program that allows users to track expenses. 
     Attributes: 
-        balance (float): the users available funds 
+        value(float): the amount to be subtracted from total
         deduction (float): the amount to be subtracted from total   
     '''
     
@@ -29,17 +29,28 @@ class ExpenseTracker:
         print(f"Amount of funds available {new_balance}")
         return (new_balance)
         
+<<<<<<< Updated upstream
     def subtraction(self): #Chika
+=======
+    def Subtraction(self, balance, deduction): #Chika
+>>>>>>> Stashed changes
         ''' Subtracts the amount of each expense from the total_amount_to_spend
             Parameters:
-                deduction (int): the amount to be deducted from the balance.
+                balance (int): 
         '''
+<<<<<<< Updated upstream
         #  call stored balance to get remaining balance  
                
         total_amount  = self.balance - self.deduction
         print(f"You new balance is {total_amount}") 
         if self.deduction > self.balance:
             print("Not enough availble funds.")
+=======
+        #  call stored balance to get remaining balance        
+        
+        total_amount  = balance - deduction 
+        print(f"You new balance is {total_amount}")   
+>>>>>>> Stashed changes
                 
     def store_balance(self,total_amount): #Ray
         ''' After user is done, saves the amount_spent to a dictionary
@@ -65,24 +76,36 @@ class ExpenseTracker:
         
     def balance_warning(self): #Christian
         '''This method will include a function that will notify the user with a 
-        balance warning (based on what they set as half and minimum funds).
+        balance warning based on what they set as half and minimum funds.
         
         Args:
-            balance: The user’s stored balance 
+            halfway_balance: When the user’s balance is half of what they 
+            initially put into the tracker
+            low_balance: When the user’s balance is as low as they indicated to 
+            avoid.
 		'''
+<<<<<<< Updated upstream
         if self.balance <= self.funds / 2:
             print(f"WARNING: You have used half of your available funds. Remaining balance: {self.balance}")
         if self.balance <= self.funds / 4:
             print(f"LOW BALANCE WARNING: You have used 75 percent of your available funds. Remaining balance: {self.balance}")  
             
+=======
+    #if else statement, if balance equals half of stored_balance
+    #make an f string for available balance of x, print warning message
+    #if balance == stored_amount/2 
+    #if stored_balance <=0 
+    
+>>>>>>> Stashed changes
     def categorize_shopping(self,shopping_list): #Christian
-        '''This method will contain a dictionary to categorize what the 
-        user will shop for. 
+        '''
+        This method will contain a dictionary with categories the user will 
+        use to divide who/what to shop for 
         
         
         Args:
             shopping_list: the dictionary that will add the categories the user 
-            indicates. 
+            inputs.
         '''
         #shopping_list = []
     
@@ -95,3 +118,4 @@ class ExpenseTracker:
     if __name__ == "__main__": #Ray
         args = parse_args(sys.argv[1:])
         
+``

fbb8d17b2fbf1c3281ec64057dbeb23d4b0f4866
Ray Swenton <RaymondSwentonJr@gmail.com>
2020-12-18 23:39:49 -0500

    Update test_expense.py

--- test_expense.py
+++ test_expense.py
@@ -8,25 +8,31 @@ def test_expense_funds(capsys):
     sure that the user is inputting an integer value"""
     with mock.patch("builtins.input",
                     side_effect = ["one, -1, 1"]):
-        x = ExpenseTracker.funds
-        assert x == "Amount of funds available 1"
+        x = ExpenseTracker()
+        assert x.funds() == 1
         captured = capsys.readouterr()
         assert captured.out == (
             "please enter int value.\n"
             "Enter a number greater than 0.\n"
         )
-def test_subtraction():
+def test_subtraction(capsys):
     """ Test the subtractions() method of the ExpenseTracker class and 
     makes sure that the ouput is calculated correctly"""
     with mock.patch("builtins.input", side_effect = 
-                    [500, "yes", 30, "yes", "fifty", 0, "five"]):
-        assert (500,100) == 400
-        assert (1000,200) == 800
-        assert (700,500) == 200
+                    ["500", "yes", "30", "yes", "fifty", "no"]):
+        ET = ExpenseTracker()
+        assert ET.subtraction() == (-530.0,[500.0, 30.0])
+        captured = capsys.readouterr()
+        assert captured.out == (
+            "You didn't enter a number for the amount spent!\n"
+            'Invalid response: Please type yes or no\n' 
+            'Your updated balance is $-530.0\n'
+        )
+        
 
 def test_balance_warning(capsys):
     ET = ExpenseTracker()
-    assert ET.balance_warning() == []
+    assert ET.balance_warning()
     captured = capsys.readouterr()
     assert captured.out == (
         f"LOW BALANCE WARNING: You have used 75 percent of your" 
@@ -37,23 +43,22 @@ def test_balance_warning(capsys):
 
 def test_categorize_shopping(capsys):
     with mock.patch("builtins.input",
-                    side_effect = ["1","Bob", "one", "Bob","3", "0"]):
+                    side_effect = ["1","Bob","3", "0"]):
         ET = ExpenseTracker()
         assert ET.categorize_shopping() == "Bob", []
         captured = capsys.readouterr()
         assert captured.out == (
+            "Enter 1 to add a name, 2 for an item, 0 when done:\n"
             "Enter who you are shopping for:\n"
-            "Enter 1 to add another name, 2 for an item, 0 when done"
-            "Not an integer value\n"
-            "Enter who you are shopping for:\n"
-            "You entered an invalid number\n"
-            "Enter 1 to add a name, 2 for an item, and 0 when done:\n"
+            "Enter 1 to add another name, 2 for an item, 0 when done:\n"
+            "Invalid number: Please enter 1 for a person, 2 for an item, 0 when" 
+            " done:\n"
         )
     
 
 def test_overdraw_amount(capsys):
     ET = ExpenseTracker()
-    assert ET.overdraw_amount()  
+    assert ET.overdraw_amount() == None 
     captured = capsys.readouterr()
     assert captured.out == (
         "Not enough left in balance.\n"
